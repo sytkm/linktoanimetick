cbb776024fd43e05d9ebefb7ea9aa3faef867e2b
sytkm <kame.adventure@gmail.com>
2016-01-10 00:42:02 +0900

    Simplify change
    remove Official Site Button:ボタンがサイトデザインに合ってないため
    add link in title
    disable link color and underline

--- animetick.user.js
+++ animetick.user.js
@@ -8,21 +8,20 @@
 
 (function() {
 	var element = document.createElement('a');
+	var tagElem = document.createElement('a');
 	var objBody = document.getElementsByClassName("anime-info-bottom")[0];
 	var tagBody = document.getElementsByClassName("hashtag")[0];
-	var tagElem = document.createElement('a');
 	var hashtag = tagBody.innerHTML;
-	element.style.backgroundColor = "#FF3307";
-	element.style.textDecoration = "none";
-	element.style.borderRadius = "5px";
-	element.style.color = "#FFFFFF";
-	element.target="_blank";
-	element.innerHTML = "Official Site";
+	element.innerHTML = objBody.children[0].innerHTML;
 	element.href = "http://www.google.com/search?btnI=I%27m+Feeling+Lucky&lr=lang_ja&ie=UTF-8&oe=UTF-8&q=" + objBody.children[0].innerHTML;
+	element.style.textDecoration = "none";
+	element.style.color = "black";
 	tagElem.innerHTML = hashtag;
-	console.log(hashtag);
 	tagElem.href = "https://twitter.com/hashtag/"+ hashtag.trim().slice(1);
-	objBody.children[1].appendChild(element);
+	tagElem.style.textDecoration = "none";
+	tagElem.style.color = "black";
+
+	objBody.children[0].replaceChild(element,objBody.children[0].firstChild);
 	tagBody.replaceChild(tagElem,tagBody.firstChild);
 
 })();
