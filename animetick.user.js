60aba9d381dfb22300952e94d416fbeb7d169653
sytkm <kame.adventure@gmail.com>
2016-01-10 00:27:18 +0900

    hashtag link to twitter

--- animetick.user.js
+++ animetick.user.js
@@ -3,18 +3,26 @@
 // @namespace      https://twitter.com/sytkm
 // @description    this javascript makes link to the official website in animetick
 // @include        http://animetick.net/anime/*
-// @version        "1.1"
+// @version        "1.2"
 // ==/UserScript==
 
 (function() {
-    var element = document.createElement('a');
-    var objBody = document.getElementsByClassName("anime-info-bottom")[0];
-    element.style.backgroundColor = "#FF3307";
-    element.style.textDecoration = "none";
-    element.style.borderRadius = "5px";
-    element.style.color = "#FFFFFF";
-    element.target="_blank";
-    element.innerHTML = "Official Site";
-    element.href = "http://www.google.com/search?btnI=I%27m+Feeling+Lucky&lr=lang_ja&ie=UTF-8&oe=UTF-8&q=" + objBody.children[0].innerHTML;
-    objBody.children[1].appendChild(element);
+	var element = document.createElement('a');
+	var objBody = document.getElementsByClassName("anime-info-bottom")[0];
+	var tagBody = document.getElementsByClassName("hashtag")[0];
+	var tagElem = document.createElement('a');
+	var hashtag = tagBody.innerHTML;
+	element.style.backgroundColor = "#FF3307";
+	element.style.textDecoration = "none";
+	element.style.borderRadius = "5px";
+	element.style.color = "#FFFFFF";
+	element.target="_blank";
+	element.innerHTML = "Official Site";
+	element.href = "http://www.google.com/search?btnI=I%27m+Feeling+Lucky&lr=lang_ja&ie=UTF-8&oe=UTF-8&q=" + objBody.children[0].innerHTML;
+	tagElem.innerHTML = hashtag;
+	console.log(hashtag);
+	tagElem.href = "https://twitter.com/hashtag/"+ hashtag.trim().slice(1);
+	objBody.children[1].appendChild(element);
+	tagBody.replaceChild(tagElem,tagBody.firstChild);
+
 })();
